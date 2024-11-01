4c15ea2c266632a56fcd5b3405ee44d8033d8673
LucasStudioLight <lucas@studiolight.com.br>
2024-11-01 01:44:11 -0300

    Adapt app for cloud deployment

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,9 @@ import os
 # Add the src directory to Python path
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
+# Set environment variable to indicate cloud deployment
+os.environ['DEPLOYMENT_ENV'] = 'cloud'
+
 # Import the main app
 from src.app import main
 
